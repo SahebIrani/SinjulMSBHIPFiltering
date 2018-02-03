718b1ca36925c1117203b9ebe2ae111a66943abf
SinjulMSBH13 <jackslater.irani@gmail.com>
2018-02-03 23:20:58 +0330

    Initial

---
+++ SinjulMSBHIPFiltering/SinjulMSBHIPFiltering/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SinjulMSBHIPFiltering.Models;
+
+namespace SinjulMSBHIPFiltering.Controllers
+{
+	public class HomeController: Controller
+	{
+		private IHttpContextAccessor _accessor;
+
+		public HomeController ( IHttpContextAccessor accessor )
+		{
+			_accessor=accessor;
+		}
+
+		public IActionResult Index ( )
+		{
+			return View( );
+		}
+
+		public IActionResult GetIP ( )
+		{
+			ViewData[ "GetIP" ] = _accessor.HttpContext.Connection.RemoteIpAddress.ToString( );
+			return View( );
+		}
+
+		public IActionResult About ( )
+		{
+			ViewData[ "Message" ] = "Your application description page.";
+
+			return View( );
+		}
+
+		public IActionResult Contact ( )
+		{
+			ViewData[ "Message" ] = "Your contact page.";
+
+			return View( );
+		}
+
+		public IActionResult Error ( )
+		{
+			return View( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
+		}
+	}
+}
